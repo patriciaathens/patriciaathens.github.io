6d57874ba0d9c770f1bf32cb4a3a144f81a19941
Patricia Acorda <acordp@rpi.edu>
2021-02-16 21:16:22 -0500

    jquery.ready

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-$('#navbar ul li a').on('click', function() {
-  $(this).parent().find('a').removeClass('active');
-  $(this).addClass('active');
-});
+$(document).ready(function () {
+  $('#navbar ul li a').on('click', function() {
+    $(this).parent().find('a').removeClass('active');
+    $(this).addClass('active');
+  });
 
-$(window).on('scroll', function() {
-  $('.section').each(function() {
-      if($(window).scrollTop() >= Math.floor($(this).position().top)) {
-        var id = $(this).attr('id');
-        $('#navbar ul li a').removeClass('active');
-        $('#navbar ul li a[href=#'+ id +']').addClass('active');
-      }
+  $(window).on('scroll', function() {
+    $('.section').each(function() {
+        if($(window).scrollTop() >= Math.floor($(this).position().top)) {
+          var id = $(this).attr('id');
+          $('#navbar ul li a').removeClass('active');
+          $('#navbar ul li a[href=#'+ id +']').addClass('active');
+        }
+    });
   });
 });
 
