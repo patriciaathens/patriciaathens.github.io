5125e7ee1e56e619067048b6df454e17222f9a7a
Patricia Acorda <acordp@rpi.edu>
2021-07-19 17:01:08 -0400

    ADD: setup of main pages

---
+++ src/pages/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.sass';
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return (
+            <React.Fragment>
+                <main>
+                    <p>Hello World</p>
+                </main>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default App;
