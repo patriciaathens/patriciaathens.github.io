d7cbab12fa73c36a957d09f6269851fedc8c9146
Patricia Acorda <acordp@rpi.edu>
2021-07-21 20:31:55 -0400

    FIX: link styling, ADD: card base

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg">
+        <nav className="navbar sticky-top navbar-expand-lg">
             <Link className="navbar-brand" to="/">Patricia Acorda</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
--- src/pages/App.js
+++ src/pages/App.js
@@ -34,9 +34,12 @@ class App extends Component {
                         </Route>
                     </Switch>
                     
-                    <main>
-                        <p>Hello World</p>
-                    </main>
+                    <div class="card">
+                    {/* <img class="card-img-top" src="" alt="Card image cap" /> */}
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    </div>
                 </Router>
                 
             </React.Fragment>
