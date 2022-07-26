b64083d4ffed2508302cb6dcc13d12ccfdb726a0
Patricia Acorda <patricia.acorda@gmail.com>
2022-07-26 18:27:58 -0400

    ADD: resume pages (hidden)

---
+++ src/components/resume.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+const About = () => {
+    return (
+        <main className="rounded d-flex justify-content-around align-items-stretch">
+            <section className="p-2">
+                <h1>Resume</h1>
+                <h2>Education</h2>
+                <p>Rensselaer Polytechnic Institute</p>
+                <p>Bachelor of Science in Computer Science & Information Technology Web Sciences</p>
+                <p>GPA: 3.24</p>
+
+                <h2>Skills</h2>
+                <h3>Sci.CORPS Instructor</h3>
+                <h3>Yale Peabody Museum</h3>
+                <ul>
+                    <li><p>Created and taught HTML & CSS curriculums for high school staff in the Sci.CORPS Program</p></li>
+                    <li><p>Managed student and self updates behind the evolution.peabody.yale.edu website</p></li>
+                    <li><p>Managed and guided 10-15 high school staff in group projects for the Yale Peabody Museum</p></li>
+                </ul>
+
+                <h2>Work Experience</h2>
+                <p>asdf</p>
+
+                <h2>Projects & Research</h2>
+                <p>asdf</p>
+
+                <h2>Awards</h2>
+                <p>asdf</p>
+            </section>
+        </main>
+    );
+}
+ 
+export default About;
